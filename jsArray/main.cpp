5c3dccb6f2c2a7a575fc14055c387f9f88301b01
alien35 <aleon6@u.rochester.edu>
2019-02-23 17:56:31 -0500

    Initial Commit

---
+++ jsArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  jsArray
+//
+//  Created by Alexander Leon on 2/23/19.
+//  Copyright © 2019 Alexander Leon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
